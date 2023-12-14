2dbe890466641b3c433b087f2c4fcb49d26e154b
barone66x <mattia.li.greci@gmail.com>
2023-12-14 09:59:58 +0100

    fix generici

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 import * as Three from "three";
 import "./style.css";
 import { FlyControls } from "three/examples/jsm/controls/FlyControls";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 let path1 = "./floor.json";
 let path2 = "./bobine.json";
@@ -51,13 +53,13 @@ const box = new Three.Mesh(
   new Three.BoxGeometry(1, 1, 1),
   new Three.MeshBasicMaterial({ color: "#ff0000" })
 );
-box.add(new Three.AxesHelper());
-box.position.y -= 1;
-box.position.z = 0;
-box.position.x = 10;
-console.log(box);
-scene.add(box);
-scene.add(new Three.AxesHelper());
+// box.add(new Three.AxesHelper());
+// box.position.y -= 1;
+// box.position.z = 0;
+// box.position.x = 10;
+// scene.add(box);
+
+// scene.add(new Three.AxesHelper());
 
 //#endregion
 
@@ -91,12 +93,6 @@ function onResize() {
   // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 }
 
-function generateBobine() {
-  bobine.forEach((f) => {
-    const bobina = new Three.Mesh(new Three.BoxGeometry());
-  });
-}
-
 function generateFloors() {
   floors.forEach((f) => {
     const floor = new Three.Mesh(
@@ -114,56 +110,101 @@ function generateFloors() {
     // console.log(f.position.x);
     // console.log(f.position.y);
 
-    let cyl = new Three.Mesh(
-      new Three.BoxGeometry(1, 1, 1),
-      new Three.MeshNormalMaterial()
-    );
+    // let cyl = new Three.Mesh(
+    //   new Three.BoxGeometry(1, 1, 1),
+    //   new Three.MeshNormalMaterial()
+    // );
 
-    cyl.position.x = f.position.x;
-    cyl.position.z = f.position.y;
+    // cyl.position.x = f.position.x;
+    // cyl.position.z = f.position.y;
 
     floor.position.x = f.position.x;
     floor.position.z = f.position.y;
 
     let dx = f.size.x / 2;
     let dy = f.size.y / 2;
-    console.log("Floor position pre");
-    console.log( floor.position.x );
+ 
     floor.position.x += dx;
-    console.log("floor position post");
-    console.log( floor.position.x );
+ 
     floor.position.z += dy;
 
     let x = floor.position.x;
     let z = floor.position.z;
-
+    
     floor.rotation.y = Three.MathUtils.degToRad(f.rotation);
-    cyl.rotation.y = -Three.MathUtils.degToRad(f.rotation);
-
+    // cyl.rotation.y = -Three.MathUtils.degToRad(f.rotation);
+    
     floor.position.x =
-      (x - f.position.x) * Math.cos(Three.MathUtils.degToRad(-f.rotation)) -
-      (z - f.position.y) * Math.sin(Three.MathUtils.degToRad(-f.rotation)) +
-      f.position.x;
-
-    console.log("posizione centro post traslazione x");
-    console.log(floor.position.x);
-
+    (x - f.position.x) * Math.cos(Three.MathUtils.degToRad(-f.rotation)) -
+    (z - f.position.y) * Math.sin(Three.MathUtils.degToRad(-f.rotation)) +
+    f.position.x;
+    
+    
+    
+    
     floor.position.z =
-      (x - f.position.x) * Math.sin(Three.MathUtils.degToRad(-f.rotation)) +
-      (z - f.position.y) * Math.cos(Three.MathUtils.degToRad(-f.rotation)) +
-      f.position.y;
-
+    (x - f.position.x) * Math.sin(Three.MathUtils.degToRad(-f.rotation)) +
+    (z - f.position.y) * Math.cos(Three.MathUtils.degToRad(-f.rotation)) +
+    f.position.y;
+    
     // scene.add(floor)
     // cyl.add(floor);
     // cyl.rotation.y += Three.MathUtils.degToRad(f.rotation);
 
     // floor.position.x -= dx * Math.cos(Three.MathUtils.degToRad(f.rotation));
     // floor.position.z -= dy * Math.sin(Three.MathUtils.degToRad(f.rotation));
-
+    
+    floor.name = f.id;
     scene.add(floor);
-    scene.add(cyl);
+    // scene.add(cyl);
   });
 }
+
+async function generateBobine(idPavimento) {
+  bobine = await loadJson(path2, idPavimento);
+  bobine.forEach(async (f) => {
+    const bobina = await loadFbx("./models/bobina2.fbx");
+    bobina.add(new Three.AxesHelper(50));
+    bobina.scale.set(0.02 * f.diameter, 0.02 * f.length, 0.02 * f.diameter);
+    bobina.rotation.z = Math.PI / 2;
+    bobina.position.y = f.diameter / 2 - 0.1 * f.diameter;
+    bobina.position.x = f.position.x;
+    bobina.position.z = f.position.y;
+    bobina.name = f.id;
+    bobina.floorId = f.floorId;
+    console.log(bobina);
+    scene.add(bobina);
+  });
+}
+
+async function loadFbx(path) {
+  const fbxLoader = new FBXLoader();
+  let x = new Three.Group();
+  x = await fbxLoader.loadAsync(path);
+
+  x.traverse(function (child) {
+    if (child.isMesh) {
+      if (child.material) {
+        child.material.transparent = false;
+      }
+    }
+  });
+
+  let i = 0;
+  while (i < x.children.length) {
+    if (x.children[i].type != "Mesh") {
+      x.children.splice(i, 1);
+      i--;
+    }
+    i++;
+  }
+  x.castShadow = false;
+  x.receiveShadow = false;
+  return x;
+
+  // && !(x.children[i].type == "PointLight")
+}
+
 
 //#region EventListener
 window.addEventListener("resize", onResize);
@@ -176,3 +217,5 @@ async function loadJson(path) {
   return res;
 }
 //#endregion
+
+
